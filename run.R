395354b1e0f25de1e3fb10767e7fd17aaf483bba
Nikolay Rovdo <nichogaus@gmail.com>
2022-06-13 12:20:01 +0400

    Update run.R

--- run.R
+++ run.R
@@ -1,12 +1,7 @@
-
-
 initial <- read.csv("rnsdataset.csv")
 dat <- initial
 
-
 # > plot(dat$dist_to_subway, dat$price) # test plot
-
-
 
 model <- lm(price ~   dist_to_subway + size_t + room_no + furniture + floor+ floors, data = dat)
 price <- dat$price
